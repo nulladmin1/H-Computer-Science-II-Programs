42ad4ea33eeef4a5a386989ad9d2b6a85343d1e7
nulladmin1 <shrey.deogade@protonmail.com>
2025-02-20 23:21:16 -0500

    Add new testing suite in Source.cpp

--- fraction/Source.cpp
+++ fraction/Source.cpp
@@ -115,19 +115,66 @@ int main()
 //
 //    read.printFraction();
 {
-    Fraction f1;
-    f1.readFraction();
 
-    cout << endl;
-    
-    Fraction f2;
-    f2.readFraction();
+  cout << "Testing: additon & subtraction: " << endl;
+  Fraction f1;
+  f1.readFraction();
+  cout << endl;
 
-    cout << "Sum: ";
-    f1.add(f2).printFraction();
+  Fraction f2;
+  f2.readFraction();
 
-    cout << "Difference: ";
-    f1.subtract(f2).simplified().printFraction();
+  cout << "Sum: ";
+  f1.add(f2).printFraction();
 
-    return 0;
+  cout << "Difference: ";
+  f1.subtract(f2).simplified().printFraction();
+
+  cout << "\nTesting: multiplication & division: " << endl;
+  Fraction f3;
+  f3.readFraction();
+  cout << endl;
+
+  Fraction f4;
+  f4.readFraction();
+
+  cout << "Product: ";
+  f3.multiply(f4).simplified().printFraction();
+
+  cout << "Quotient: ";
+  f3.divide(f4).simplified().printFraction();
+
+  cout << "\nTesting: comparison: " << endl;
+  Fraction f5;
+  f5.readFraction();
+  cout << endl;
+
+  Fraction f6;
+  f6.readFraction();
+
+  cout << "Equal?: ";
+  if (f5.isEqual(f6))
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+
+  cout << "First greater than second?: ";
+  if (f5.isGreater(f6))
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+
+  cout << "First less than second?: ";
+  if (f5.isLess(f6))
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+
+  cout << "Testing simplification: " << endl;
+  Fraction f7;
+  f7.readFraction();
+  cout << "Simplified: ";
+  f7.simplified().printFraction();
+
+  return 0;
 }
